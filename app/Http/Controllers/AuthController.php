58098d61ca5d9edfe08146fdcb22758e90179bc6
Faizaapauzan <ithinkfaiza.mfgsoln@gmail.com>
2022-08-01 12:11:50 +0800

    faiza 1/8/2022

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -38,7 +38,7 @@ class AuthController extends Controller
       
               if(!$user || !Hash::check($request['password'], $user->password)) {
                 return response([
-                    'message' => 'Username & Password does not match'
+                    'message' => 'Username & Password does not match with our record.'
                 ], 401);
         }
     

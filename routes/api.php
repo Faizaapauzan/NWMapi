055d8f152505ecd48fdbb962451b243ed40a6572
Faizaapauzan <ithinkfaiza.mfgsoln@gmail.com>
2022-10-07 09:27:42 +0800

    faiza 7/10/2022

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomepageController;
 use App\Http\Controllers\AssignJobController;
+use App\Http\Controllers\UnassignedController;
 
 /*
 |--------------------------------------------------------------------------
@@ -32,6 +33,8 @@ Route::group(['middleware' => ['auth:sanctum']], function () {
 
     Route::get('/todoassignedjob', [AssignJobController::class, 'todo']);
     Route::get('/doingassignedjob', [AssignJobController::class, 'doing']);
+
+    Route::get('/UnassignedJob', [UnassignedController::class, 'UnassignedJob']);
 });
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
